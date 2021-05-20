81cbba0a38300934d39ff7df59d95c8803ebf82d
Nelly Sugu <nelly.sugu@gmail.com>
2021-05-20 17:36:15 -0400

    unsplash photo

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ let { destinations } = require("./db");
 
 // import (require) express function
 const express = require("express");
+const fetch = require("node-fetch");
+require("dotenv").config();
 
-const { uid, name } = require("./services");
+const { uid } = require("./services");
 
 // call the express function to create our HTTP server
 // => the created server is "deaf"
@@ -30,14 +32,14 @@ app.get("/destinations", (req, res) => {
 
 // POST => CREATE
 // Expect the client to send us an object
-// {name, location, photo?, description?}
+// {name, location, , description?}
 // name and location are required
 app.post("/destinations", (req, res) => {
-  const { name, location, photo, description } = req.body;
+  const { name, location, description } = req.body;
   // const userData = req.body
   // const name = userData.name
   // const location = userData.location
-  // const photo = userData.photo
+
   // const description = userData.description
 
   // validate that we have a name and a location
@@ -50,29 +52,71 @@ app.post("/destinations", (req, res) => {
     return res.status(400).send({ error: "name and location are required" });
   }
 
-  // add the user data in my db
-  destinations.push({
-    id: uid(),
-    name: name,
-    location: location,
-    photo: photo ? photo : "",
-    description: description ? description : "",
+  // Get a photo using the name and location from Unsplash
+  // => make an API request to Unsplash to search for photos related to our name and location
+  // URL
+  const URL = `https://api.unsplash.com/search/photos/?client_id=${process.env.API_KEY}&query=${name} ${location}`;
+
+  fetch(URL)
+    .then((response) => response.json())
+    .then((photos) => {
+      // do something with the photos
+      const array = photos.results;
+
+      // pick a random photo from my array
+      const randIdx = Math.floor(Math.random() * array.length);
+      const photo = array[randIdx].urls.small;
+
+      // add the user data in my db
+      destinations.push({
+        id: uid(),
+        name: name,
+        location: location,
+        photo: photo,
+        description: description ? description : "",
+      });
+
+      res.send({ status: "success" });
+    });
+});
+
+// DELETE /destination/:id
+app.delete("/destination/:id", (req, res) => {
+  const { id } = req.params;
+
+  const filtered = destinations.filter((dest) => {
+    if (dest.id !== id) {
+      return true;
+    }
   });
+
+  destinations = filtered;
 
   res.send({ status: "success" });
 });
 
-// DELETE /destination/:uid
-// app.delete("/destination/:id", (req, res) => {
-//   const { id } = req.params;
+// PUT /destination/:id
+app.put("/destination/:id", (req, res) => {
+  const { id } = req.params;
 
-//   const filtered = destinations.filter((dest) => {
-//     if (dest.id !== id) {
-//       return true;
-//     }
-//   });
+  const { name, location, photo, description } = req.body;
 
-//   destinations = filtered;
+  if (!name && !location && !photo && !description) {
+    return res.status(400).json({ status: "no data to update" });
+  }
 
-//   res.send({ status: "success" });
-// });
+  for (let dest of destinations) {
+    if (dest.id === id) {
+      // if (name !== undefined || name.length !== 0){
+      //   dest.name = name
+      // }
+      dest.name = name ? name : dest.name;
+      dest.location = location ? location : dest.location;
+      dest.photo = photo ? photo : dest.photo;
+      dest.description = description ? description : dest.description;
+      break;
+    }
+  }
+
+  res.send({ status: "success" });
+});
